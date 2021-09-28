43892268b2653bb0868e13f163d27fc6ad59fad3
Jason Bailey <jmbailey816@gmail.com>
2021-09-27 22:04:26 -0400

    Slower animation of Hero Image -- video at 2:20:27

--- react--movie--db/src/components/HeroImage/HeroImage.styles.js
+++ react--movie--db/src/components/HeroImage/HeroImage.styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   background: linear-gradient(
@@ -11,7 +11,7 @@ export const Wrapper = styled.div`
   background-position: center;
   height: 600px;
   position: relative;
-  animation: animateHeroImage 1s;
+  animation: animateHeroImage 3s;
 
   @keyframes animateHeroImage {
     from {
