df99afe42539c0ae2adc4e98cdd0531f7c416528
Jason Bailey <jmbailey816@gmail.com>
2021-09-27 20:23:35 -0400

    Header and Global Styles

--- react--movie--db/src/App.js
+++ react--movie--db/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 
+// components 
+import Header from "./components/Header";
+
+// styles
+import { GlobalStyle } from "./GlobalStyle";
+
 
 function App() {
-  return <div className="App">
-    Start here.
-    </div>;
+  return (
+    <div className="App">
+      <Header />
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
