50f5c162fda33efbe27b80f4b6141a747fee6e44
Jason Bailey <jmbailey816@gmail.com>
2021-09-28 16:09:05 -0400

    Routing functional

--- react--movie--db/src/App.js
+++ react--movie--db/src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 
-// components 
+// routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
 // styles
 import { GlobalStyle } from "./GlobalStyle";
 
+const App = () => (
+  <Router>
+    <Header />
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
+    <Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Routes>
 
-      <GlobalStyle />
-    </div>
-  );
-}
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
