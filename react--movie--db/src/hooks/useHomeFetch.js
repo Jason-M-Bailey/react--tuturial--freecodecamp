2b3901c76f7208c1ef89a787ab8c297f5841158f
Jason Bailey <jmbailey816@gmail.com>
2021-09-28 11:41:21 -0400

    Search bar progress

--- react--movie--db/src/hooks/useHomeFetch.js
+++ react--movie--db/src/hooks/useHomeFetch.js
@@ -10,9 +10,12 @@ const initialState = {
 
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  console.log(searchTerm)
 
   const fetchMovies = async (page, searchTerm = "") => {
     try {
@@ -42,5 +45,5 @@ export const useHomeFetch = () => {
   // todo: why the empty array?
   // only runs once
 
-  return { state, loading, error };
+  return { state, loading, error, setSearchTerm };
 };
